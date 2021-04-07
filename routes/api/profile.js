9cdaa00e9a20a1bd557263ad8eedd5126dae639b
Dan Elis <danelis19@gmail.com>
2021-04-07 17:57:22 +0200

    added 4 routes

---
+++ routes/api/profile.js
@@ -0,0 +1,12 @@
+//fetching/adding/updating profiles
+
+const express = require('express');
+const router = express.Router();
+
+// @route  GET api/profile
+// @desc   Test route
+// @access Public
+
+router.get('/', (req, res) => res.send('Profile route'));
+
+module.exports = router;
